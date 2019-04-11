88bc02fc2334bfc604eb21f534b699418fceb6cd
Umbrien <kalugin.serej@gmail.com>
2019-04-11 18:31:27 +0300

    created Login & signup screens

---
+++ app/src/main/java/com/pinkertone/pinkercardsmvp/signUpActivity.java
@@ -0,0 +1,20 @@
+package com.pinkertone.pinkercardsmvp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void jumpToLogin(View view) {
+        Intent intent = new Intent(signUpActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
